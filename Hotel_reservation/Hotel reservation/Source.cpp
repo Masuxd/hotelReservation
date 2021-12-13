4c73281cdb7517baa865094b9b4d31561c35e6f7
Masuxd <matias.tuovinen@tuni.fi>
2021-12-13 19:19:27 +0200

    small additions

--- Hotel_reservation/Hotel reservation/Source.cpp
+++ Hotel_reservation/Hotel reservation/Source.cpp
@@ -6,19 +6,34 @@
 using namespace std;
 
 //functions
-void room_check();  //check if room is taken or not
-void random();
+void room_check();  //check if room is taken or not.
+bool random();  //random checkin room generator.
+
+
+struct reservation {
+	string first_name;
+	string last_name;
+	int room_number;
+	int check_in_number;
+};
 
 
 int main() {
 	/* Here I make the main menu where you can select what you want to do.
-	Also calling other parts of code here so they can choose what they want to do*/
-	int options;
+	Also calling other parts of code here so they can choose what they want to do.*/
+	int options, i, reservations;
+	const int max_index = 300;
 	bool error, menu = true;
+	reservation res[max_index];
+
+		/*if (reservation > max_index) {
+			cout << "All rooms are reserved.";
+
+		}
+		*/
 
 
-
-	//Simple main menu for choosing options
+	//Simple main menu for choosing options.
 	cout << "_______________________________________________________" << endl;
 	cout << "	 Welcome to the hotel reservation" << endl;
 	cout << "_______________________________________________________" << endl;
@@ -29,9 +44,9 @@ int main() {
 
 
 
-	// checking the input
+	// error checker gotten from moodle but modified little bit to get it working.
 	do {
-		//Simple main menu for choosing options
+		//Simple main menu for choosing options.
 		cout << "_______________________________________________________" << endl;
 		cout << "	 Welcome to the hotel reservation" << endl;
 		cout << "_______________________________________________________" << endl;
@@ -43,6 +58,8 @@ int main() {
 		cin >> options;
 		error = false;
 
+
+		//gives error message if input is wrong
 		if (cin.fail()) {
 			cout << "Error. Enter your number again!" << endl;
 			cin.clear();
@@ -50,15 +67,26 @@ int main() {
 			continue;
 		}
 
-
+		//simple switch case what is the menu what I will be using.
 			switch (options) {
 
 				case 1:
 					cout << "______________________________________________________________" << endl;
 					cout << "		Room reservation" << endl;
 					cout << "______________________________________________________________" << endl;
+					cout << "Please type in first name here: ";
+					cin >> res[i].first_name;
+					
+					cout << "Please type in last name here: ";
+					cin >> res[i].last_name;
 
 					random();
+					res[i].check_in_number = random();;
+
+					i++;
+
+		
+
 					cout << endl;
 					break;
 
@@ -87,20 +115,20 @@ int main() {
 	return 0;
 }
 
-void random() {
+bool random() {
 	int reservation_number, a, b, c, d, e;
 
-	srand(time(NULL));
-
+	srand(time(0));
+	//this generates diffrent numbers from 0-9. I multiply them to to get one big number that we can use give room check-in numbers. 
 	a = 10000 * (rand() % 10);
 	b = 1000 * (rand() % 10);
 	c = 100 * (rand() % 10);
 	d = 10 * (rand() % 10);
 	e = 1 * (rand() % 10);
 	reservation_number = a + b + c + d + e;
+	
+	//just prints our equation seen above.
+	cout << "Your reservation number is" << reservation_number << endl;
 
-	cout << reservation_number << endl;
-
-
-
+	return reservation_number;
 }
